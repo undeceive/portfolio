032073d41509591f08059f7f83adbb35ce6a0318
undeceive <GitHubClient@protonmail.com>
2022-07-03 12:04:55 -0400

    Sanity & React

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,21 +3,24 @@ import { motion } from 'framer-motion';
 
 import { images } from '../../constants';
 import './About.scss';
-
-const abouts = [
-  { title: 'Front end', description: 'I am a good web developer', imgUrl: images.about01},
-  { title: 'Back end', description: 'I am a good web developer', imgUrl: images.about02},
-  { title: 'Full stack', description: 'I am a good web developer', imgUrl: images.about03},
-  { title: 'Junior Java Developer', description: 'I am a good web developer', imgUrl: images.about04}
-];
+import { urlFor, client } from '../../client';
 
 const About = () => {
+  const [abouts, setAbouts] = useState ([]);
+
+  useEffect (() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+          .then((data) => setAbouts(data))
+  }, []);
+
   return (
     <>
       <h2 className="head-text">I know that <span>Good Apps</span> <br /> means <span>Good Business</span></h2>
 
       <div className="app__profiles">
-        {abouts.map((about, index)=>(
+        {abouts.map((about, index)=> (
           <motion.div
               whileInView={{ opacity:1 }}
               whileHover={{ scale: 1.1 }}
@@ -25,7 +28,7 @@ const About = () => {
               className="app__profile-item"
               key={about.title + index}
             >
-              <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about.imgUrl)} alt={about.title} />
               <h2 className="bold-text" style={{marginTop: 20}}>{about.title}</h2>
               <p className="p-text" style={{marginTop: 10}}>{about.description}</p>
             </motion.div>
